e9b39e257515a015db3d14f5e3a41a55f3808a23
cyd losekann <cydnel@gmail.com>
2022-10-18 01:08:29 -0300

    night mode switch button

---
+++ components/styled-components - modo noturno/App.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import React from 'react';
+
+const Button = styled.button`
+  background: ${({ ativo }) => (ativo ? '#ff003a' : '#000')};
+  border: 1px solid black;
+  font-size: 1rem;
+  padding: 0.5rem;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
+`;
+
+const Container = styled.div`
+  background-color: ${({ ativo }) => (ativo ? '#17405f' : '#fff')};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const App = () => {
+  const [ativo, setAtivo] = React.useState(false);
+
+  return (
+    <Container ativo={ativo} onClick={() => setAtivo(!ativo)}>
+      <Button ativo={ativo} onClick={() => setAtivo(!ativo)}>
+        {ativo ? 'Modo noturno ativado' : 'Ativar modo noturno'}
+      </Button>
+  </Container>
+  );
+};
+
+export default App;
