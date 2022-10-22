769d3ac3dec1a976c45ae6a8ce71b5ecdad32a57
cyd losekann <cydnel@gmail.com>
2022-10-21 23:35:07 -0300

    routes

---
+++ components/Router/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Sobre from './Sobre';
+import Pagina404 from './Pagina404';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sobre" element={<Sobre />} />
+        <Route path="*" element={<Pagina404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
