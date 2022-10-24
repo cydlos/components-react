ed5b8bad94e91b3ce646177270ec743a1a52c003
cyd losekann <cydnel@gmail.com>
2022-10-24 01:54:03 -0300

    footer and header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import React from 'react'
+import Produtos from './Components/Produtos'
 
 const App = () => {
   return (
--- src/Components/Produtos.js
+++ src/Components/Produtos.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './Produtos.module.css'
 
 export const Produtos = () => {
   return (
