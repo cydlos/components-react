57ea651557edae3e0a512c5cf21994d1e1f6c0da
cyd losekann <cydnel@gmail.com>
2022-10-24 22:33:40 -0300

    navlink added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,13 +3,13 @@ import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <nav classname="styles.header">
+    <nav className="styles.header">
       <ul>
         <li>
-          <NavLink to="/" end></NavLink>
+          <NavLink className="styles.link" to="/" end>Produtos</NavLink>
         </li>
         <li>
-          <NavLink to="contato">Contato</NavLink>
+          <NavLink className="styles.link"  to="contato">Contato</NavLink>
         </li>
       </ul>
     </nav>
