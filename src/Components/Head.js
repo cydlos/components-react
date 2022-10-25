9a8ecb28b19c9c0128558d3b0c5ae5a75542ebbc
cyd losekann <cydnel@gmail.com>
2022-10-25 19:38:59 -0300

    refactoring

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -3,9 +3,10 @@ import React from 'react'
 export const Head = (props) => {
   React.useEffect(() => {
     document.title = props.title
-    document.querySelector('meta[name="description"]').setAttribute('content', props.description)
+    .querySelector('meta[name="description"]')
+    .setAttribute('content', props.description);
   }, [props])
-  return <></>
-}
+  return <></>;
+};
 
 export default Head
