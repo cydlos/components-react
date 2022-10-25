b3d67245697370bd15d62785be2363aa2c1f8214
cyd losekann <cydnel@gmail.com>
2022-10-25 16:04:12 -0300

    contact page with css

--- src/Components/Contato.js
+++ src/Components/Contato.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import styles from './Contato.module.css'
+import foto from '../img/typewriter.jpeg'
 
 export const Contato = () => {
   return (
-    <section className={styles.contato}>Contato</section>
+    <section className={styles.contato}>
+      <img src={foto} alt="Typewriter" />
+      <div>
+        <h1>Contato</h1>
+        <ul>
+          <li>e-mail: blabla@gmail.com</li>
+          <li>phone number: 9999-9999</li>
+          <li>address: Blabla St. 498</li>
+        </ul>
+      </div>
+    </section>
   )
 }
 
