07b899d3b0ec6e9f8e002a861651ab5d3b6f2645
cyd losekann <cydnel@gmail.com>
2022-10-25 23:46:44 -0300

    API fetch working

--- src/Components/Produtos.js
+++ src/Components/Produtos.js
@@ -3,15 +3,23 @@ import styles from './Produtos.module.css'
 
 const Produtos = () => {
 
+  const [produtos, setProdutos] = React.useState(null)
+
   React.useEffect(() => {
     fetch('https://ranekapi.origamid.dev/json/api/produto')
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => setProdutos(json))
   }, []);
+
+  if (produtos === null) return null;
   return (
-    <div className={styles.produto}>
-      <h1>Produtos</h1>
-    </div>
+    <section className={styles.produto + ' animeLeft'}> {produtos.map((produto => (
+      <div key={produto.id}>
+        <h1>{produto.nome}</h1>
+        <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
+      </div>
+    )))}
+    </section>
   )
 }
 
