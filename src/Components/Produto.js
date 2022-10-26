2e20e2674ab1f0fb34d2edba2fb8dc5e60fc5d28
cyd losekann <cydnel@gmail.com>
2022-10-26 04:04:32 -0300

    product

--- src/Components/Produto.js
+++ src/Components/Produto.js
@@ -1,28 +1,43 @@
-import React from 'react'
-import styles from './Produto.module.css'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import styles from "./Produto.module.css";
+import { useParams } from "react-router-dom";
+import Produtos from "./Produtos";
 
 const Produto = () => {
-  const [produto, setProduto] = React.useState(null)
-  const [loading, setLoading] = React.useState(false)
-  const [error, setError] = React.useState(null)
+  const [produto, setProduto] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
   const { id } = useParams();
 
-  React.useEffect (() => {
+  React.useEffect(() => {
     async function fetchProduto(url) {
-      setLoading(true);
-      const response = await fetch(url);
-      const json = await response.json();
-      setProduto(json);
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        const json = await response.json();
+        setProduto(json);
+      } catch (error) {
+        setError("Ocorreu um erro.");
+      } finally {
+        setLoading(false);
+      }
     }
     fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
   }, [id]);
 
+  if (loading) return <div className={styles.loading}>Carregando...</div>;
+  if (error) return <p> {error} </p>
+  if (produto === null ) return null;
   return (
-    <div className={styles.produto}>
-      <h1>Produto</h1>
-    </div>
-  )
-}
+    <section className={styles.produto}>
+      {produto.foto.map ((foto) => <img src={foto.src} alt={foto.titulo} />)}
+      <div>
+          <h1> {produto.nome}</h1>
+          <span className={styles.preco}>R$ {produto.preco}</span>
+          <p className={styles.descricao}>{produto.descricao}</p>
+      </div>
+    </section>
+  );
+};
 
-export default Produto
+export default Produto;
