1ff5d977adef22217fb6035a75b3282c43e70135
cyd losekann <cydnel@gmail.com>
2022-10-25 23:30:03 -0300

    fetch API

--- src/Components/Produtos.js
+++ src/Components/Produtos.js
@@ -2,6 +2,12 @@ import React from 'react'
 import styles from './Produtos.module.css'
 
 const Produtos = () => {
+
+  React.useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto')
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }, []);
   return (
     <div className={styles.produto}>
       <h1>Produtos</h1>
